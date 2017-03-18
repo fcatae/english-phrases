ce172fa71e10c521acc760cdd600d219b781e442
Fabricio Catae <fcatae@microsoft.com>
2017-03-18 01:35:18 -0300

    define anki controller

---
+++ english/Controllers/AnkiController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace english.Controllers
+{
+    [Route("api/[controller]")]
+    public class AnkiController : Controller
+    {
+        [HttpPost("start")]
+        public void Start([FromBody]object userInfo)
+        {
+        }
+
+        [HttpGet("{session_id}/question")]
+        public string Question(string session_id, string user)
+        {
+            return "";
+        }
+
+        [HttpGet("{session_id}/answer")]
+        public string Answer(string session_id, int question_id)
+        {
+            return "value";
+        }
+
+        [HttpPost("{session_id}/rate")]
+        public void Rate(int session_id, [FromBody]object questionRating)
+        {
+        }
+    }
+}
