c6bec8c4b556ddcc8649c98a8fe76c5786998788
Fabricio Catae <fcatae@microsoft.com>
2017-03-18 10:41:22 -0300

    test events

--- english/wwwroot/index.js
+++ english/wwwroot/index.js
@@ -1,3 +1,23 @@
+
+// - start session
+// - get question
+// - show answer key
+// - rate the question
+// - restart
+
+// jQuery Controller
+$(function () {
+
+    $('#btn_answer').click( ()=> { alert('show answer') });
+    $('#question_text').text('The boy and the girl are studying today');
+    $('#answer_text').text('O menino e a menina estão estudando hoje');
+    $('.answerrating').click( (ev)=> { 
+        let button = ev.target
+        alert('rate: ' + button.value) });
+
+});
+
+// API
 $(function () {
 
     var phrase = "it just works";
@@ -10,12 +30,3 @@ $(function () {
     });
 
 });
-
-function phrase_submit(text) {
-
-    // send the text    
-    alert(text);
-    // receive the id
-    
-    return false;
-}
