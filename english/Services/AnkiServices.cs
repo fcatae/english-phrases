8f0a670e57626f62e6669cc3e73f336a2b5117ec
Fabricio Catae <fcatae@microsoft.com>
2017-03-18 18:36:06 -0300

    cleanup

--- english/Services/AnkiServices.cs
+++ english/Services/AnkiServices.cs
@@ -17,55 +17,34 @@ namespace english.Services
 
     class AnkiServices : IAnkiServices
     {
+        public AnkiServices()
+        {            
+        }
+
         public int StartSession(string user, bool isFirstLogin)
         {
-            if( user == "test" ) 
-            {
-                return (isFirstLogin) ? 50001 : 60009;
-            }
-
-            return -1;
+            return 1;
         }
 
         public int GetRandomQuestion(string user)
         {
-            int INVALID_QUESTION_ID = -1;
-
-            return ( user == "test" ) ? 123 : INVALID_QUESTION_ID;
+            // get next GetRandomQuestion
+            return 2;
         }
 
         public string GetQuestion(int questionId)
         {
-            switch(questionId)
-            {
-                case 123: 
-                    return "(test_question) Is this a real test?";
-                case -1:
-                    return "who are you?";                
-            }
-            
             return "what is your question?";
         }
 
         public string GetAnswer(int questionId)
         {
-            switch(questionId)
-            {
-                case 123: 
-                    return "(test_answer) The answer is correct";
-                case -1:
-                    return "negative value - very weird question";
-            }
-            
             return "(test_answer) No, the answer is wrong";
         }
 
         public void RateQuestion(int question_id, int rating)
         {
-            if( question_id == 123 && rating >= 1 && rating <= 100 )
-            {                
-            }
-            else throw new InvalidOperationException("failed test");
+            new InvalidOperationException("failed test");
         }
 
     }
