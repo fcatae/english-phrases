1714e18215965fd2c06bd147c1c34c46393179da
Fabricio Catae <fcatae@microsoft.com>
2017-03-18 14:44:18 -0300

    page load

--- english/wwwroot/index.js
+++ english/wwwroot/index.js
@@ -5,12 +5,74 @@
 // - rate the question
 // - restart
 
+// state
 var page_state = {
     question_text: null,
     answer_text: null,
-    display_rating_buttons: false
+    display_rating_buttons: false,
+
+    user: null,
+    session_id: null,
+    question_id: null
 };
 
+let state_start_session = () => ({
+        question_text: null,
+        answer_text: null,
+        display_rating_buttons: false,
+        user: 'test',
+        session_id: null,
+        question_id: null        
+    });
+
+let state_session_started = (state, session_id) => {
+        state.session_id = state;
+        return state;
+    };
+
+let state_question_received = (state, question_id, question_text) => {
+        state.question_id = question_id;
+        state.question_text = question_text;        
+        return state;
+    };
+
+// event: init
+(function page_init() {
+
+    page_state = state_start_session();
+
+})();
+
+// event: load
+$(function page_load() {
+
+    ankiAPI.start(page_state.user, /*isFirstLogin*/ true)
+        .done( (session_id) => {
+            page_state = state_session_started(page_state, session_id);
+
+            ankiAPI.question(session_id, page_state.user)
+            .done( (qr) => {
+                page_state = state_question_received(page_state,
+                                                        qr.question_id,
+                                                        qr.question_text);
+                render(page_state);                                                        
+            });
+
+        });    
+
+})
+
+// event: btn_answer -> click
+$('#btn_answer').click( ()=> {
+     
+});
+
+// event: answerrating -> click
+$('.answerrating').click( (ev)=> {
+     let button = ev.target
+});
+
+// accessors
 let set_question_text = (text) => $('#question_text').text(text);
 let show_answer_button = (show) => $('#btn_answer').show();
 let hide_answer_button = (show) => $('#btn_answer').hide();
@@ -20,6 +82,7 @@ let set_answer_text = (text) => $('#answer_text').text(text);
 let show_rating_buttons = (show) => $('.answerratebox').show();
 let hide_rating_buttons = (show) => $('.answerratebox').hide();
 
+// render
 function render(page) {
 
     if(page.question_text == null) { 
@@ -85,5 +148,5 @@ $(function () {
         display_rating_buttons: true
     });
 
-    // render(state_???);
+    // render(state);
 });
