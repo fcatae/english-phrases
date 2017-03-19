5f129fecc8ee2400d11783a239cf997087775074
Fabricio Catae <fcatae@microsoft.com>
2017-03-19 10:49:12 -0300

    add tests to vs2017 (wip)

---
+++ test/anki.ServicesTests/ATests.fs
@@ -0,0 +1,8 @@
+module Tests2
+
+open System
+open Xunit
+
+[<Fact>]
+let ``My test2`` () =
+    Assert.True(true)
---
+++ test/anki.ServicesTests/AnkiServicesTests.fs
@@ -0,0 +1,13 @@
+module Tests3
+
+open System
+open Xunit
+open english.Services
+
+[<Fact>]
+let ``Initial`` () =
+    let a = new AnkiServices()
+    
+    Assert.True(false)
+
+    
