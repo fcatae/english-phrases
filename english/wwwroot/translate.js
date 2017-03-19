3a4f03f5a63130f5adf4879ce20adc1f8da057ed
Fabricio Catae <fcatae@microsoft.com>
2017-03-19 18:56:45 -0300

    created the translate UI

--- english/wwwroot/translate.js
+++ english/wwwroot/translate.js
@@ -1,46 +1,34 @@
 
-// - start session
-// - get question
-// - show answer key
-// - rate the question
-// - restart
+
+ankiAPI.pendingAnswer = function() {
+    return $.when({
+        question_id: 123,
+        question_text: 'testing..?'
+    });
+}
+
+ankiAPI.translate = function(question_id, answer_text) {
+    alert(answer_text)
+    return $.when();
+}
 
 // state
 var page_state = {
     question_text: null,
-    answer_text: null,
-    display_rating_buttons: false,
-
-    user: null,
-    session_id: null,
     question_id: null
 };
 
 let state_start_session = () => ({
         question_text: null,
-        answer_text: null,
-        display_rating_buttons: false,
-        user: 'test',
-        session_id: null,
         question_id: null        
     });
 
-let state_session_started = (state, session_id) => {
-        state.session_id = state;
-        return state;
-    };
-
 let state_question_received = (state, question_id, question_text) => {
-        state.question_id = question_id;
         state.question_text = question_text;        
+        state.question_id = question_id;
         return state;
     };
 
-let state_answer_received = (state, answer_text) => {
-        state.answer_text = answer_text;
-        state.display_rating_buttons = true;
-        return state;
-    };
 
 // event: init
 (function page_init() {
@@ -49,75 +37,46 @@ let state_answer_received = (state, answer_text) => {
 
 })();
 
-function startSession(isFirstLogin) {
+// accessors
+let set_question_text = (text) => $('#question_text').text(text);
+let get_answer_text = () => $('#answer_text').val();
+let set_answer_text = (text) => $('#answer_text').val(text);
+
+function startSession() {
+
+    set_answer_text('');
 
     page_state = state_start_session();
+    render(page_state);
 
-    ankiAPI.start(page_state.user, isFirstLogin)
-        .done( (session_id) => {
-            page_state = state_session_started(page_state, session_id);
+    ankiAPI.pendingAnswer()
+    .done( (qr) => {
+        page_state = state_question_received(page_state,
+                                                qr.question_id,
+                                                qr.question_text);
+        render(page_state);
+    });
 
-            ankiAPI.question(session_id, page_state.user)
-            .done( (qr) => {
-                page_state = state_question_received(page_state,
-                                                        qr.question_id,
-                                                        qr.question_text);
-                render(page_state);                                                    
-            });
-
-        });        
 }
+
 // event: load
 $(function page_load() {
 
-    ankiAPI.start(page_state.user, /*isFirstLogin*/ true)
-        .done( (session_id) => {
-            page_state = state_session_started(page_state, session_id);
+    startSession();
 
-            ankiAPI.question(session_id, page_state.user)
-            .done( (qr) => {
-                page_state = state_question_received(page_state,
-                                                        qr.question_id,
-                                                        qr.question_text);
-                render(page_state);                                                    
-            });
-
-        });    
-
-})
+});
 
 // event: btn_answer -> click
 $('#btn_answer').click( ()=> {
      
-     ankiAPI.answer(page_state.session_id, page_state.user, page_state.question_id)
-        .done( (answer) => {
-            page_state = state_answer_received(page_state, answer);
-            render(page_state);                                                    
+     var text = get_answer_text();
+
+     ankiAPI.translate(page_state.question_id, text)
+        .done( () => {
+            startSession();                
         });
 
 });
-
-// event: answerrating -> click
-$('.answerrating').click( (ev)=> {
-     let button = ev.target
-
-     let rating = parseInt(button.value);
-
-     ankiAPI.rate(page_state.session_id, page_state.user, page_state.question_id, rating)
-        .done( ()=>{
-            startSession(/*isFirstLogin*/ false);
-        });   
-});
-
-// accessors
-let set_question_text = (text) => $('#question_text').text(text);
-let show_answer_button = (show) => $('#btn_answer').show();
-let hide_answer_button = (show) => $('#btn_answer').hide();
-let show_answer_text = (show) => $('#answer_text').show();
-let hide_answer_text = (show) => $('#answer_text').hide();
-let set_answer_text = (text) => $('#answer_text').text(text);
-let show_rating_buttons = (show) => $('.answerratebox').show();
-let hide_rating_buttons = (show) => $('.answerratebox').hide();
 
 // render
 function render(page) {
@@ -128,42 +87,7 @@ function render(page) {
     }
 
     set_question_text(page.question_text);
-
-    if(page.answer_text == null) {
-        show_answer_button();
-        hide_answer_text();
-    } else {
-        hide_answer_button();
-        set_answer_text(page.answer_text);
-        show_answer_text();
-    }
-
-    if(page.display_rating_buttons == true) {
-        show_rating_buttons();
-    } else {
-        hide_rating_buttons();
-    }
 }
-
-function page_loop(state) {
-    render(state);
-}
-
-$(function() {
-
-    function ignore_now() {
-    $('#btn_answer').click( ()=> { alert('show answer') });
-    $('.answerrating').click( (ev)=> { 
-        let button = ev.target
-        alert('rate: ' + button.value) });
-    }
-
-});
-
-// start tests
-$(function() {
-    testAnki( ankiAPI );
-})
 
 // render tests
 $(function () {
