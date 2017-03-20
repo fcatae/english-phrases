0831e800a5378ee5e91a9b2bbe886630ffb516e0
Fabricio Catae <fcatae@microsoft.com>
2017-03-19 21:11:54 -0300

    reset

---
+++ english/Controllers/ResetController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using english.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace english.Controllers
+{
+    [Route("reset")]
+    public class ResetController : Controller
+    {
+        EnglishContext _db;
+
+        public ResetController(EnglishContext db)
+        {
+            this._db = db;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            // Delete
+            _db.Database.EnsureDeleted();
+
+            // Create
+            _db.Database.EnsureCreated();
+
+            return "Done";
+        }
+        
+    }
+}
