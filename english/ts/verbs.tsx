ae925d5b1858900545a6807f2d1dd6896c2fe1cc
Fabricio Catae <fcatae@microsoft.com>
2017-03-28 22:40:58 -0300

    initial impl

--- english/ts/verbs.tsx
+++ english/ts/verbs.tsx
@@ -1,25 +1,59 @@
 
-class AppVerb extends React.Component<{},{}> {
+interface AppVerbProps {
+    present: string,
+    past: string,
+    perfect: string,
+
+    show_present: boolean,
+    show_past: boolean,
+    show_perfect: boolean    
+}
+
+class AppVerb extends React.Component<AppVerbProps,{}> {
+
+   answer() {
+        alert(1)
+   }
+
    render() {
       return (
          <div>
-             <div className="verbheader col-md-8 col-md-offset-2">
-                <div className="row">
-                    <span className="col-md-4">Present</span>
-                    <span className="col-md-4">Past</span>
-                    <span className="col-md-4"> Present Perfect</span>
-                </div>
-            </div>
-
+            <form autoFocus>
              <div className="verbtable col-md-8 col-md-offset-2">
                 <div className="row">
-                    <span className="col-md-4">Present</span>
-                    <span className="col-md-4">Past</span>
-                    <span className="col-md-4"> Present Perfect</span>
-                </div>
+                    <span className="col-md-4"><VerbText ref="p1" show={this.props.show_present} label="Present" answer={this.props.present}/></span>
+                    <span className="col-md-4"><VerbText ref="p2" show={this.props.show_past} label="Past" answer={this.props.past}/></span>
+                    <span className="col-md-4"><VerbText ref="p3" show={this.props.show_perfect} label="Present Perfect" answer={this.props.perfect}/></span>
+                </div>                
             </div>
 
+            <div className="verbsubmit col-md-12">
+            <button type="submit" className="btn btn-lg btn-primary" onClick={this.answer}>Submit</button>
+                
+            </div>
+           </form>
          </div>
       );
    }
+}
+
+interface VerbTextProps {
+    answer: string;
+    label: string;
+    show: boolean;
+}
+
+class VerbText extends React.Component<VerbTextProps,{}> {
+    render() {
+
+        let showAnswer = this.props.show;
+        let label = this.props.label;
+        let verb = this.props.answer;
+
+        if(showAnswer) {
+            return <p className="form-control-static"><strong>{ verb }</strong></p>;            
+        }
+
+        return <input className="form-control text-center" type="text" placeholder={label} />;
+    }
 }
