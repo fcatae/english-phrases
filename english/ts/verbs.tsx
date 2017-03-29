079ad6ee0898644829c437e5ec31459c1d22d313
Fabricio Catae <fcatae@microsoft.com>
2017-03-28 21:18:09 -0300

    grid

--- english/ts/verbs.tsx
+++ english/ts/verbs.tsx
@@ -3,7 +3,22 @@ class AppVerb extends React.Component<{},{}> {
    render() {
       return (
          <div>
-            Hello verbs!
+             <div className="verbheader col-md-8 col-md-offset-2">
+                <div className="row">
+                    <span className="col-md-4">Present</span>
+                    <span className="col-md-4">Past</span>
+                    <span className="col-md-4"> Present Perfect</span>
+                </div>
+            </div>
+
+             <div className="verbtable col-md-8 col-md-offset-2">
+                <div className="row">
+                    <span className="col-md-4">Present</span>
+                    <span className="col-md-4">Past</span>
+                    <span className="col-md-4"> Present Perfect</span>
+                </div>
+            </div>
+
          </div>
       );
    }
