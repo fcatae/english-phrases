16516df5f25bc18e7bde3f30222b60798ea9a695
Fabricio Catae <fcatae@microsoft.com>
2017-03-28 23:05:30 -0300

    proper binding

--- english/ts/verbs.tsx
+++ english/ts/verbs.tsx
@@ -11,8 +11,14 @@ interface AppVerbProps {
 
 class AppVerb extends React.Component<AppVerbProps,{}> {
 
+   private p1: any;
+   private p2: any;
+   private p3: any;
+
    answer() {
-        alert(1)
+        alert(this.p1.userAnswer());
+        alert(this.p2.userAnswer());
+        alert(this.p3.userAnswer());
    }
 
    render() {
@@ -21,14 +27,14 @@ class AppVerb extends React.Component<AppVerbProps,{}> {
             <form autoFocus>
              <div className="verbtable col-md-8 col-md-offset-2">
                 <div className="row">
-                    <span className="col-md-4"><VerbText ref="p1" show={this.props.show_present} label="Present" answer={this.props.present}/></span>
-                    <span className="col-md-4"><VerbText ref="p2" show={this.props.show_past} label="Past" answer={this.props.past}/></span>
-                    <span className="col-md-4"><VerbText ref="p3" show={this.props.show_perfect} label="Present Perfect" answer={this.props.perfect}/></span>
+                    <span className="col-md-4"><VerbText ref={c=> this.p1=c} show={this.props.show_present} label="Present" answer={this.props.present}/></span>
+                    <span className="col-md-4"><VerbText ref={c=> this.p2=c} show={this.props.show_past} label="Past" answer={this.props.past} /></span>
+                    <span className="col-md-4"><VerbText ref={c=> this.p3=c} show={this.props.show_perfect} label="Present Perfect" answer={this.props.perfect}/></span>
                 </div>                
             </div>
 
             <div className="verbsubmit col-md-12">
-            <button type="submit" className="btn btn-lg btn-primary" onClick={this.answer}>Submit</button>
+            <button type="submit" className="btn btn-lg btn-primary" onClick={this.answer.bind(this)}>Submit</button>
                 
             </div>
            </form>
@@ -44,6 +50,13 @@ interface VerbTextProps {
 }
 
 class VerbText extends React.Component<VerbTextProps,{}> {
+    
+    private input: any;
+
+    userAnswer() {
+        return (this.props.show) ? this.props.answer : this.input.value;
+    }
+
     render() {
 
         let showAnswer = this.props.show;
@@ -54,6 +67,6 @@ class VerbText extends React.Component<VerbTextProps,{}> {
             return <p className="form-control-static"><strong>{ verb }</strong></p>;            
         }
 
-        return <input className="form-control text-center" type="text" placeholder={label} />;
+        return <input className="form-control text-center" type="text" placeholder={label} ref={input=>this.input=input}/>;
     }
 }
